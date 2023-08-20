1e59d70f0fa358fb2ef355e01e312ad53e04fe0f
tudor.ulmeanu <tudor.ulmeanu@nhm.ac.uk>
2023-08-20 15:44:52 +0100

    [TU] Initial commit

---
+++ src/main/java/comcheckout/CheckoutComApplication.java
@@ -0,0 +1,13 @@
+package comcheckout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckoutComApplication.class, args);
+    }
+
+}
---
+++ src/test/java/comcheckout/CheckoutComApplicationTests.java
@@ -0,0 +1,13 @@
+package comcheckout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
